61d28ed6931b16092da4dfcb6db8ae6a1a6a8d6a
Oscar Cortez <om.cortez.2010@gmail.com>
2015-10-10 19:20:05 -0600

    return json parse

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import {NicSvg} from './model/nic-svg';
 
 global.app = function (element, config_path) {
     var nicaragua = new NicSvg(element, config_path);
-    nicaragua.svgToDom();
+    nicaragua.build();
 };
