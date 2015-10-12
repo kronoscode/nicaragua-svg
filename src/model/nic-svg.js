b58814e394907839ded0f57335d502237e90a891
Oscar Cortez <om.cortez.2010@gmail.com>
2015-10-11 19:22:43 -0600

    create function for get json config

--- src/model/nic-svg.js
+++ src/model/nic-svg.js
@@ -1,5 +1,7 @@
 const injectSvg = require('svg-injector');
 
+import { get } from '../util/utils.js';
+
 class NicSvg {
     constructor(svgElem, jsonPath) {
         this.svgElem = svgElem;
@@ -8,6 +10,22 @@ class NicSvg {
 
     svgToDom() {
         return injectSvg(document.getElementById(this.svgElem));
+    }
+
+    getJSON(url = this.jsonPath) {
+      return get(url)
+      .then(function() {
+        return JSON.parse(url);
+      })
+      .catch(function(err) {
+        console.log("getJSON failed for", url, err);
+        throw err;
+      });
+    }
+
+    build() {
+      this.getJSON();
+      this.svgToDom();
     }
 
 }
