04c2c60df02358dac84170acca47194ded19a629
Oscar Cortez <om.cortez.2010@gmail.com>
2015-11-14 15:40:05 -0600

    getjson after the svg elem is render

--- src/model/nic-svg.js
+++ src/model/nic-svg.js
@@ -1,21 +1,19 @@
 const injectSvg = require('svg-injector');
 
-import { get } from '../util/utils.js';
+import { get } from '../utils/get.js';
+import { dom } from '../utils/dom.js';
 
 class NicSvg {
+
     constructor(svgElem, jsonPath) {
         this.svgElem = svgElem;
         this.jsonPath = jsonPath;
     }
 
-    svgToDom() {
-        return injectSvg(document.getElementById(this.svgElem));
-    }
-
     getJSON(url = this.jsonPath) {
       return get(url)
-      .then(function() {
-        return JSON.parse(url);
+      .then(function(config) {
+        return dom(JSON.parse(config));
       })
       .catch(function(err) {
         console.log("getJSON failed for", url, err);
@@ -23,8 +21,19 @@ class NicSvg {
       });
     }
 
+    svgToDom() {
+      let _this = this;
+      return injectSvg(
+              document.getElementById(this.svgElem),
+              { evalScripts: 'once' },
+              function () {
+                // call getJSON once all the requested SVG elements have been injected.
+                _this.getJSON();
+              }
+            );
+    }
+
     build() {
-      this.getJSON();
       this.svgToDom();
     }
 
