0ce67fc85aed3fd5d25c971cc9d1ad7b5bb6ce53
Oscar Cortez <om.cortez.2010@gmail.com>
2015-11-14 16:26:56 -0600

    finish dom function

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -3,11 +3,18 @@ export function dom(object) {
   for ( let depto in config ) {
 
     let parent = document.getElementById(depto);
+
+    parent.setAttribute('xlink:href', config[depto].link)
+    parent.setAttribute('target', config[depto].target)
+
     let dep = parent.getElementsByTagName('path')[0] || parent.getElementsByTagName('polygon')[0]
     
     // TODO: Refactor this 
     dep.setAttribute('class', config[depto].class);
     dep.setAttribute('fill', config[depto].fill);
+    dep.setAttribute('stroke', config[depto].stroke)
+    dep.setAttribute('stroke-width', config[depto].stroke_width)
+
 
   }
 
